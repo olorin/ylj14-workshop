45c4e5e1286814439f9212ee376efd26facfbf75
Sharif Olorin <sio@tesser.org>
2014-05-08 16:20:43 +1000

    gensum solution

--- gensum/Main.hs
+++ gensum/Main.hs
@@ -5,11 +5,19 @@ import Language.C.Quote.C
 import Language.Haskell.TH        (Name, mkName)
 import Text.PrettyPrint.Mainland
 
+--genSum :: Name -> Int -> BlockItem
+--genSum arr n = [citem| {
+--                 int sum = 0;
+--                 for (int i = 0; i++; i < $n)
+--                   sum += $id:(show arr)[i];
+--               } |]
+
 genSum :: Name -> Int -> BlockItem
 genSum arr n = [citem| {
-                 int sum = 0;
-                 for (int i = 0; i++; i < $n)
-                   sum += $id:(show arr)[i];
-               } |]
+                int sum = 0;
+                $stms:statements
+    } |]
+  where
+    statements = map (\i -> [cstm| sum += $id:(show arr)[$int:i]; |]) [0,1 .. n]
                
 main = print $ ppr (genSum (mkName "arr") 5)
