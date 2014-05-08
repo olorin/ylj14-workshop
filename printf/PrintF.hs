1c834884ecf5e3dd62069816a84aed79c11ec5a0
Sharif Olorin <sio@tesser.org>
2014-05-08 15:44:51 +1000

    Generalised gen function

--- printf/PrintF.hs
+++ printf/PrintF.hs
@@ -16,13 +16,21 @@ data Format = D | S | L String
 
 -- The printf meta-programming function
 printf :: String -> ExpQ
-printf fmt = gen (parse fmt)
+printf fmt = gen2 (parse fmt) [| "" |]
 
 -- Generating Haskell code for a particular format
 gen :: [Format] -> ExpQ
 gen [D]   = [| \n -> show n |]
 gen [S]   = [| \s -> s |]
 gen [L s] = [| s |]
+
+
+gen2 :: [Format] -> ExpQ -> ExpQ
+gen2 [] acc    = acc
+gen2 (x:xs) acc = case x of 
+    D -> [| \n -> $(gen2 xs [| $acc ++ (show n) |]) |]
+    S -> [| \s -> $(gen2 xs [| $acc ++ s |]) |]
+    L s -> [| $(gen2 xs [| $acc + s |]) |]
 
 -- A poor man's tokenizer
 parse :: String -> [Format]
@@ -33,29 +41,3 @@ parse (s:str) = L (s:p) : parse rest -- so we don't get stuck on weird '%'
     where (p, rest) = span (/= '%') str
 
 
-{-
--- generate argument list for the function
-args :: [Format] -> [PatQ]
-args fmt = concatMap (\(f,n) -> case f of
-                                  L _ -> []
-                                  _   -> [varP n]) $ zip fmt names
-    where names = [ mkName $ 'x' : show i | i <- [0..] ]
-
--- generate body of the function
-body :: [Format] -> ExpQ
-body fmt = foldr (\ e e' -> infixApp e [| (++) |] e') (last exps) (init exps)
-    where exps = [ case f of
-                    L s -> stringE s
-                    D   -> appE [| show |] (varE n)
-                    S   -> varE n
-                 | (f,n) <- zip fmt names ]
-          names = [ mkName $ 'x' : show i | i <- [0..] ]
-
--- glue the argument list and body together into a lambda
--- this is what gets spliced into the haskell code at the call
--- site of "printf"
-printf :: String -> Q Exp
-printf format = lamE (args fmt) (body fmt)
-    where fmt = tokenize format    
-    -}
-    
